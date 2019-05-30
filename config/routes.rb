b0ef5e86f3d63870e61dcfe40bbf8756a5b5eca5
Nurbakhyt <nurbakhyt@gmail.com>
2019-05-30 12:53:26 +0600

    feature: auth w/ github

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
-  root 'welcome#index'
+  root 'auth#index'
+  get '/auth/github', to: 'auth#login'
+  get '/auth/github/cb', to: 'auth#cb'
+  delete 'logout', to: 'auth#logout', as: 'logout'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
